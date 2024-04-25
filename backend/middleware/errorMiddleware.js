bdcc9b5ac88a7f402c891cde48ef587c4a985dcc
Dank3m <dankimist@gmail.com>
2024-04-25 08:54:37 +0300

    "Add user registration, logout, and profile routes; implement error handling middleware"
    This diff adds new user-related routes for registration, logout, and getting and updating the user profile. It also adds error handling middleware to

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -3,3 +3,21 @@ const notFound = (req, res, next) => {
     res.status(404);
     next(error)
 }
+
+const errorHandler = (err, req, res, next) => {
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let message = err.message;
+
+    if (err.name === 'CastError' && err.kind === 'ObjectId' ) {
+        statusCode =404;
+        message = `Resource Not Found.`;
+
+    }
+
+    res.status(statusCode).json({
+        message: message,
+        stack: process.env.NODE_ENV === "production" ? "" : err.stack
+    });
+}
+
+export {notFound , errorHandler};
