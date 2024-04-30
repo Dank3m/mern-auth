3a9c10e1ab84a7f8632d9fbb5a823bb8d4517446
Dank3m <dankimist@gmail.com>
2024-04-30 07:41:27 +0300

    Update user controller and delete user model
    - Update `userController.js` to authenticate user, generate token, and handle logout.
    - Delete `userModal.js` which contained user schema and pre-save hook

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,28 @@
 import asyncHandler from "express-async-handler";
 import generateToken from "../utils/generateToken.js";
-import User from "../models/userModal.js";
+import User from "../models/userModel.js";
 
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
 
 const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPasswords(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+
   res.status(200).json({ message: "Auth User" });
 });
 
@@ -47,6 +63,10 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access Public
 
 const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
   res.status(200).json({ message: "Logout User" });
 });
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -37,6 +37,10 @@ userSchema.pre("save", async function save(next) {
   }
 });
 
+userSchema.methods.matchPasswords = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
