9e655211dec4a0fb29479101ceafb7aad15067b3
Dank3m <dankimist@gmail.com>
2024-05-08 07:47:09 +0300

    Update ProfileScreen with user data management and form for updating user info; add useUpdateUserMutation hook in usersAPISlice

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -23,7 +23,8 @@ const router = createBrowserRouter(
       <Route index element={<HomeScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
-      {/* Private Routes */}
+      {/* Private Routes */
+      }
       <Route path="" element={<PrivateRoute />}>
         <Route path="/profile" element={<ProfileScreen />} />
       </Route>
@@ -37,4 +38,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <RouterProvider router={router} />
     </React.StrictMode>
   </Provider>
-);
+); 
